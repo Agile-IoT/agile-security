912b36b3f4e9f7a5078706b1a962a0a0ddc087e8
david <nopbyte>
2016-08-19 12:09:35 +0200

    handle passport errors by hand... log them if something goes wrong during loading...

--- agile-idm-web-ui/auth/api/bearer-strategy.js
+++ agile-idm-web-ui/auth/api/bearer-strategy.js
@@ -4,23 +4,27 @@ var connectionPoolPromisse = require('../token-connection-pool');
 
 
 connectionPoolPromisse.then(function(storage){
+ try{
+       passport.use(new BearerStrategy(
+        function(token, done) {
+          storage.getTokenByToken(token, function(result){
+            if(result.hasOwnProperty("success") &&  result.success){
+               return done(null, result.data);
+            }
+            else{
+               console.log('cannot store token '+JSON.stringify(result));
+               return done(result.error);
+            }
 
-     passport.use(new BearerStrategy(
-      function(token, done) {
-        storage.getTokenByToken(token, function(result){
-          if(result.hasOwnProperty("success") &&  result.success){
-             return done(null, result.data);
-          }
-          else{
-             console.log('cannot store token '+JSON.stringify(result));
-             return done(result.error);
-          }
+          });
+        }
+      ));
+      console.log('finished registering passport bearer strategy');
 
-        });
-      }
-    ));
-    console.log('finished registering passport bearer strategy');
-
+  }catch(e){
+    console.log('FAIL TO register a strategy');
+    console.error("ERROR: error loading passport strategy: "+e);
+  }
 }, function(error){
       console.log('cannot load database error'+JSON.stringify(error));
 })
--- agile-idm-web-ui/auth/providers/github-strategy.js
+++ agile-idm-web-ui/auth/providers/github-strategy.js
@@ -5,55 +5,60 @@ var connectionPoolPromisse = require('../token-connection-pool');
 
 connectionPoolPromisse.then(function(storage){
   //strategy :)
+  try{
+      passport.use(new GithubStrategy({
+        clientID: conf.auth.github.clientID,
+        clientSecret: conf.auth.github.clientSecret,
+        callbackURL: conf.auth.github.redirect_path,
+        scope: conf.auth.google.scope
+        },
+        function(accessToken, refreshToken, profile, done) {
+          storage.getTokenByUserId(profile.id,function(result){
+              if(result.success){
+                      console.log("result from deserialize data"+ JSON.stringify(result.data));
+                      done(null, result.data);
+            }
+            else {
+              //console.log(JSON.stringify(profile));
+              var cookie_id=Math.random().toString();
+             cookie_id=cookie_id.substring(2,cookie_id.length);
+              var id=Math.random().toString();
+             id=cookie_id.substring(2,cookie_id.length);
+              token={};
+              token["user_id"]= profile.username;
+              //TODO fix date ... it doesn't expire
+              token["expiration"]= null;
+              token["scope"]=JSON.stringify(conf.auth.github.scope);
+              token["token_type"] = "bearer";
+              token["auth_type"] = "github";
+              token['token'] = accessToken;
 
-  passport.use(new GithubStrategy({
-    clientID: conf.auth.github.clientID,
-    clientSecret: conf.auth.github.clientSecret,
-    callbackURL: conf.auth.github.redirect_path,
-    scope: conf.auth.google.scope
-    },
-    function(accessToken, refreshToken, profile, done) {
-      storage.getTokenByUserId(profile.id,function(result){
-          if(result.success){
-                  console.log("result from deserialize data"+ JSON.stringify(result.data));
-                  done(null, result.data);
-        }
-        else {
-          //console.log(JSON.stringify(profile));
-          var cookie_id=Math.random().toString();
-         cookie_id=cookie_id.substring(2,cookie_id.length);
-          var id=Math.random().toString();
-         id=cookie_id.substring(2,cookie_id.length);
-          token={};
-          token["user_id"]= profile.username;
-          //TODO fix date ... it doesn't expire
-          token["expiration"]= null;
-          token["scope"]=JSON.stringify(conf.auth.github.scope);
-          token["token_type"] = "bearer";
-          token["auth_type"] = "github";
-          token['token'] = accessToken;
+              //console.log('cookie '+cookie_id)
+              //console.log('id '+id)
+              storage.storeToken(id, cookie_id, "github",token,function(result){
+                 if(result.hasOwnProperty("success") &&  result.success){
+                  console.log('github token stored '+ id)
+                  token.id = token["user_id"];
+                  console.log('github user returned  '+ JSON.stringify(token));
 
-          //console.log('cookie '+cookie_id)
-          //console.log('id '+id)
-          storage.storeToken(id, cookie_id, "github",token,function(result){
-             if(result.hasOwnProperty("success") &&  result.success){
-              console.log('github token stored '+ id)
-              token.id = token["user_id"];
-              console.log('github user returned  '+ JSON.stringify(token));
+                   return done(null,token);
+                 }
+                 else{
+                    console.log('cannot store token '+JSON.stringify(result));
+                    return done(result.error);
 
-               return done(null,token);
-             }
-             else{
-                console.log('cannot store token '+JSON.stringify(result));
-                return done(result.error);
-
-             }
+                 }
+              });
+            }
           });
         }
-      });
-    }
-  ));
-  console.log('finished registering passport github strategy');
+      ));
+      console.log('finished registering passport github strategy');
+
+  }catch(e){
+    console.log('FAIL TO register a strategy');
+    console.error("ERROR: error loading passport strategy: "+e);
+  }
 }, function(error){
     console.log('cannot load database error'+JSON.stringify(error));
 })
