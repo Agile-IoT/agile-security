de7eb335e264cbf6f29e5066a0e75def8f59b58f
nopbyte <nopbyte@users.noreply.github.com>
2016-09-07 08:59:10 +0200

    add default user to configuration

--- example/conf/agile-ui-conf.js
+++ example/conf/agile-ui-conf.js
@@ -8,26 +8,29 @@ module.exports = {
 		"createTables":true
 	},
 	"auth":{
- 			  "github":{
-						"clientID": "getGithubId",
-						"clientSecret": "getGithubSecret",
-						"redirect_path": "http://localhost:3000/callback_github",
-						"scope": ["notifications"]
-       },
-       "dropbox":{
-            "clientID":  "getDropboxId",
-            "clientSecret": "getDropboxSecret",
-            "redirect_path": "http://localhost:3000/callback_dropbox",
-						"scope": [""]
-       },
-       "google":{
-						 "clientID": "getGoogleId",
-						 "clientSecret": "getGoogleSecret",
-						 "redirect_path": "http://localhost:3000/callback_google",
-						 "scope": ["https://www.googleapis.com/auth/drive","https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email"]
-       },
-		"pam":{
-
+ 		"github":{
+				"clientID": "getGithubId",
+				"clientSecret": "getGithubSecret",
+				"redirect_path": "http://localhost:3000/callback_github",
+				"scope": ["notifications"]
+       		},
+       		"dropbox":{
+            			"clientID":  "getDropboxId",
+            			"clientSecret": "getDropboxSecret",
+            			"redirect_path": "http://localhost:3000/callback_dropbox",
+				"scope": [""]
+       		},
+       		"google":{
+				 "clientID": "getGoogleId",
+				 "clientSecret": "getGoogleSecret",
+				 "redirect_path": "http://localhost:3000/callback_google",
+				 "scope": ["https://www.googleapis.com/auth/drive","https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email"]
+       		},
+       		"local":{
+				"fallback-user-no-pam":{
+					"username":"admin",
+					"password":"correcthorsebatterystaple"
+				}
 		},
 		"web-id":{
 		}
