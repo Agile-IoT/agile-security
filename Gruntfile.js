02e1dbcc253d25876d978513dfc234b3aa1046d9
david <nopbyte>
2016-11-16 18:11:47 +0100

    add  beautify and jshint checks

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jsbeautifier: {
+      modify: {
+        src: ['Gruntfile.js', 'lib/**.js', 'tests/**.js', 'example/*.js'],
+        options: {
+          config: '.jsbeautifyrc'
+        }
+      },
+      verify: {
+        src: ['Gruntfile.js', 'lib/**.js', 'tests/**.js', 'example/*.js'],
+        options: {
+          mode: 'VERIFY_ONLY',
+          config: '.jsbeautifyrc'
+        }
+      }
+    },
+    jshint: {
+      all: ['Gruntfile.js', 'lib/**.js', 'test/**.js', 'example/*.js']
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-jsbeautifier");
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('modify', [
+    'jsbeautifier:modify',
+    'jshint'
+  ]);
+
+  grunt.registerTask('verify', [
+    'jsbeautifier:verify',
+    'jshint'
+  ]);
+  // Default task(s).
+  grunt.registerTask('default', ['verify']);
+
+};
