a0202dba4fdfeda330fdf419648db5c3e622504f
david <nopbyte>
2016-11-23 17:13:04 +0100

    move folders of example to the main folder as a normal web app

---
+++ conf/agile-idm-core-conf.js
@@ -0,0 +1,44 @@
+module.exports = {
+   "storage":{
+        "dbName":"./database_"
+   },
+   "authentication":{
+       "web-server":"http://127.0.0.1:3000/api/",
+
+  },"schema-validation":[
+    {
+      "id": "/Sensor",
+      "type": "object",
+      "properties": {
+        "name": {"type": "string"},
+        "token": {"type": "string"}
+      },
+      "required": ["name"]
+    },
+    {
+      "id": "/SimpleAddress",
+      "type": "object",
+      "properties": {
+        "lines": {
+          "type": "array",
+          "items": {"type": "string"}
+        },
+        "zip": {"type": "string"},
+        "city": {"type": "string"},
+        "country": {"type": "string"}
+      },
+      "required": ["country","lines"]
+    },
+    {
+      "id": "/SimplePerson",
+      "type": "object",
+      "properties": {
+        "name": {"type": "string"},
+        "address": {"$ref": "/SimpleAddress"},
+        "votes": {"type": "integer", "minimum": 1}
+      },
+      "required":["address"]
+
+    }
+  ]
+}
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var RouterProviers = require('./lib/routes/provider-routes');
-var RouterApi = require('./lib/routes/api-routes');
+var RouterProviers = require('./routes/provider-routes');
+var RouterApi = require('./routes/api-routes');
 var serializer =  require('./lib/auth/providers/serializer');
 var authStrategies =require('./lib/auth/providers/strategies');
 var apiStrategies = require('./lib/auth/api/bearer-strategy');
