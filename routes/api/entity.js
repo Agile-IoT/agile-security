cf86ec8575e6db51645fe136c4aae22d6e5b43fb
david <nopbyte>
2016-11-30 17:36:09 +0100

    group api in rest

--- routes/api/entity.js
+++ routes/api/entity.js
@@ -69,69 +69,6 @@ function RouterApi(idmcore, router) {
     }
   );
 
-  //returns 400 if a field of the body is missing. returns 200 and the entity, or 401 or 403, in case of security issues, 422 in case a user is attempted to be created through this API, or 409 if entity already exists, 500 in case of unexpected situations
-  //curl -H "Content-type: application/json" -H "Authorization: bearer HeTxINCpXD0U6g27D7AIxc2CvfFNaZ" -X POST -d '{"user_name":"a", "auth_type":"github"}' 'http://localhost:3000/api/v1/user'
-  router.route('/user/').post(
-    cors(),
-    passport.authenticate('bearer', {
-      session: false
-    }),
-    bodyParser.json(),
-    function (req, res) {
-      if (!req.body.user_name || !req.body.auth_type) {
-        res.statusCode = 400;
-        return res.json({
-          "error": "provide user_name and auth_type at least"
-        });
-      } else {
-        var user = req.body;
-        var entity_type = "/user";
-        var entity_id = ids.buildId(req.body.user_name, req.body.auth_type);
-        idmcore.createEntity(req.user, entity_id, entity_type, user)
-          .then(function (read) {
-            res.json(read);
-          }).catch(function (error) {
-            console.log("error when posting entity " + error);
-            res.statusCode = error.statusCode;
-            res.json({
-              "error": error.message
-            });
-          });
-      }
-    }
-  );
-
-  //returns 200 and the entity, or 401 or 403, in case of security issues, 400 in case there is not enough parameters. or 409 if entity already exists, 500 in case of unexpected situations
-  //curl -H "Authorization: bearer ypr24DKllIbKlV3Ph8oWmZ7Pml3Wku" 'http://localhost:3000/api/v1/user/?user_name=bob&auth_type=agile-local'
-  router.route('/user/').get(
-    cors(),
-    passport.authenticate('bearer', {
-      session: false
-    }),
-    bodyParser.json(),
-    function (req, res) {
-      var entity_type = "/user";
-      if(!req.query.user_name || !req.query.auth_type){
-        res.statusCode = 400;
-        return res.json({
-          "error": "provide user_name and auth_type at least as query parameters"
-        });
-      }
-      var entity_id = req.params.entity_id;
-      idmcore.readEntity(req.user, ids.buildId(req.query.user_name, req.query.auth_type), entity_type)
-        .then(function (read) {
-          res.json(read);
-        }).catch(function (error) {
-          console.log("error when reading user " + error);
-          res.statusCode = error.statusCode;
-          res.json({
-            "error": error.message
-          });
-        });
-
-    }
-  );
-
   return router;
 }
 module.exports = RouterApi;
--- routes/api/group.js
+++ routes/api/group.js
@@ -8,6 +8,64 @@ var ids = require('../../lib/util/id');
 var idmcore;
 
 function RouterApi(idmcore,router) {
+  //example to call tthis one
+  //  returns 200 and the group, or 401 or 403, in case of security issues, or  400 if parameters are missing. 500 in case of unexpected situations
+
+  // curl -H "Authorization: bearer nNGNryDDZ4zQYeWYGYcnOdxJ90k9s6" 'http://localhost:3000/api/v1/entity/user/bob!@!agile-local'
+  //returns entity with 200 if OK, else, it can return 404 if the entity is not found, 401 or 403 in case of security errors or 500 in case of unexpected situations
+  router.route('/group/').get(
+    cors(),
+    passport.authenticate('bearer', {
+      session: false
+    }),
+    function (req, res) {
+      if (!req.query.group_name || !req.query.owner) {
+        res.statusCode = 400;
+        return res.json({
+          "error": "provide group_name and owner at least"
+        });
+      } else {
+        idmcore.readGroup(req.user, req.query.group_name, req.query.owner)
+          .then(function (read) {
+            res.json(read);
+          }).catch(function (error) {
+            res.statusCode = error.statusCode;
+            res.json({
+              "error": error.message
+            });
+          });
+      }
+  });
+
+  //returns 200 and the group, or 401 or 403, in case of security issues, or 409 if the group already exists, 400 if parameters are missing. 500 in case of unexpected situations
+  //curl -H "Content-type: application/json" -H "Authorization: bearer ypr24DKllIbKlV3Ph8oWmZ7Pml3Wku" -d '{"group_name":"me", "owner":"nopbyte@github"}' 'http://localhost:3000/api/v1/group/'
+  router.route('/group/').post(
+    cors(),
+    passport.authenticate('bearer', {
+      session: false
+    }),
+    bodyParser.json(),
+    function (req, res) {
+      var group = req.body;
+      if (!req.body.group_name) {
+        res.statusCode = 400;
+        return res.json({
+          "error": "provide group_name in the body"
+        });
+      } else {
+
+        idmcore.createGroup(req.user, req.body.group_name)
+          .then(function (read) {
+            res.json(read);
+          }).catch(function (error) {
+            console.log("error when posting entity " + error);
+            res.statusCode = error.statusCode;
+            res.json({
+              "error": error.message
+            });
+          });
+      }
+  });
 
 }
 module.exports = RouterApi;
