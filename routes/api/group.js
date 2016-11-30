18eed988cfdc3710a4f14da0245edd62ad0827db
david <nopbyte>
2016-11-30 18:24:33 +0100

    add membership actions to api

--- routes/api/group.js
+++ routes/api/group.js
@@ -9,23 +9,18 @@ var idmcore;
 
 function RouterApi(idmcore,router) {
   //example to call tthis one
-  //  returns 200 and the group, or 401 or 403, in case of security issues, or  400 if parameters are missing. 500 in case of unexpected situations
-
-  // curl -H "Authorization: bearer nNGNryDDZ4zQYeWYGYcnOdxJ90k9s6" 'http://localhost:3000/api/v1/entity/user/bob!@!agile-local'
+  //  returns 200 and the group, or 401 or 403, in case of security issues. 500 in case of unexpected situations
+  //this line returns the group with name me, owned by bob who is authenticated with agile-local authentication type
+  // curl -H "Content-type: application/json" -H "Authorization: bearer ypr24DKllIbKlV3Ph8oWmZ7Pml3Wku" 'http://localhost:3000/api/v1/user/bob!@!agile-local/group/me/'
   //returns entity with 200 if OK, else, it can return 404 if the entity is not found, 401 or 403 in case of security errors or 500 in case of unexpected situations
-  router.route('/group/').get(
+  router.route('/user/:owner/group/:name').get(
     cors(),
     passport.authenticate('bearer', {
       session: false
     }),
     function (req, res) {
-      if (!req.query.group_name || !req.query.owner) {
-        res.statusCode = 400;
-        return res.json({
-          "error": "provide group_name and owner at least"
-        });
-      } else {
-        idmcore.readGroup(req.user, req.query.group_name, req.query.owner)
+
+        idmcore.readGroup(req.user, req.params.name, req.params.owner)
           .then(function (read) {
             res.json(read);
           }).catch(function (error) {
@@ -34,7 +29,7 @@ function RouterApi(idmcore,router) {
               "error": error.message
             });
           });
-      }
+
   });
 
   //returns 200 and the group, or 401 or 403, in case of security issues, or 409 if the group already exists, 400 if parameters are missing. 500 in case of unexpected situations
@@ -53,7 +48,6 @@ function RouterApi(idmcore,router) {
           "error": "provide group_name in the body"
         });
       } else {
-
         idmcore.createGroup(req.user, req.body.group_name)
           .then(function (read) {
             res.json(read);
@@ -65,6 +59,52 @@ function RouterApi(idmcore,router) {
             });
           });
       }
+  });
+
+  //returns 200 and the group, or 401 or 403, in case of security issues, or 409 if the entity is  already in the group. or 404 in case the entity or the group are not found. 400 if parameters are missing. 500 in case of unexpected situations
+  //this line adds the entity of type sensor and id 1, to the group called me and owned by bob who is authenticated with agile-local
+  //curl -H "Content-type: application/json" -H "Authorization: bearer ypr24DKllIbKlV3Ph8oWmZ7Pml3Wku" -X POST 'http://localhost:3000/api/v1/user/bob!@!agile-local/group/me/entities/sensor/1'
+  router.route('/user/:owner/group/:name/entities/:entity_type/:id').post(
+    cors(),
+    passport.authenticate('bearer', {
+      session: false
+    }),
+    bodyParser.json(),
+    function (req, res) {
+
+        idmcore.addEntityToGroup(req.user, req.params.name, req.params.owner, req.params.id, "/"+req.params.entity_type)
+          .then(function (read) {
+            res.json(read);
+          }).catch(function (error) {
+            console.log("error when posting entity " + error);
+            res.statusCode = error.statusCode;
+            res.json({
+              "error": error.message
+            });
+          });
+  });
+
+  //returns 200 and the group, or 401 or 403, in case of security issues, or 409 if the entity is not  in the group. or 404 in case the entity or the group are not found. 400 if parameters are missing. 500 in case of unexpected situations
+  //this line adds the entity of type sensor and id 1, to the group called me and owned by bob who is authenticated with agile-local
+  //curl -H "Content-type: application/json" -H "Authorization: bearer ypr24DKllIbKlV3Ph8oWmZ7Pml3Wku" -X DELETE 'http://localhost:3000/api/v1/user/bob!@!agile-local/group/me/entities/sensor/1'
+  router.route('/user/:owner/group/:name/entities/:entity_type/:id').delete(
+    cors(),
+    passport.authenticate('bearer', {
+      session: false
+    }),
+    bodyParser.json(),
+    function (req, res) {
+
+        idmcore.removeEntityFromGroup(req.user, req.params.name, req.params.owner, req.params.id, "/"+req.params.entity_type)
+          .then(function (read) {
+            res.json(read);
+          }).catch(function (error) {
+            console.log("error when posting entity " + error);
+            res.statusCode = error.statusCode;
+            res.json({
+              "error": error.message
+            });
+          });
   });
 
 }
