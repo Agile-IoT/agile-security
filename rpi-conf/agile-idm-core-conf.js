f294dde7cca875003f88b1da679467c0edd4d1ab
koustabhdolui <koustabh.here@gmail.com>
2017-06-29 16:00:34 +0200

    Add credentials to rpi-conf

--- rpi-conf/agile-idm-core-conf.js
+++ rpi-conf/agile-idm-core-conf.js
@@ -341,7 +341,14 @@ var conf = {
       "username": "agile",
       "auth_type": "agile-local",
       "role":"admin",
-      "password": "secret"
+      "password": "secret",
+      "credentials":{
+        "xively":{
+          "xivelymaster":"NU9grueAtYdQE0L7DdFlID3NBZuQn7tyyNvjXUvqoQnJ2rox",
+          "xivelyproduct":"Y1o-jUXIj66T1Ekb_Tjx",
+          "xivelysecret":"067d1c0ad522fa0315782888b4cf89741b0369ec"
+          }
+        }
     },
     "client": {
       "id": "AGILE-OSJS",
