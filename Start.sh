ca6bce3a973e437dbaf2cc3c8ef48a33c1cc82a1
david <nopbyte@users.noreply.github.com>
2017-07-10 15:06:56 +0200

    add pdp to configuration files on start

--- Start.sh
+++ Start.sh
@@ -2,9 +2,15 @@
 
 CONF=/root/idm.conf/
 
+
 if [ ! -f "$CONF/agile-idm-core-conf.js" ]; then
   echo "folder not there for conf"
   cp -r rpi-conf/* $CONF
+fi
+
+if [ ! -f "$CONF/agile-pdp-conf.js" ]; then
+  echo "folder not there for conf"
+  cp -r rpi-conf/agile-pdp-conf.js $CONF/agile-pdp-conf.js
 fi
 
 node /opt/agile-idm-web-ui/app.js $CONF
