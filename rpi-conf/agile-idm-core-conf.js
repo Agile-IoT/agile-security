e67b2c08d1a7506bebedee68d85594b938fdd8ef
david <nopbyte@users.noreply.github.com>
2017-07-28 16:33:31 +0200

    use idm-core with new policy format, after refactoring the pdp out of idm-core and adding the recursive deletion of entities

--- rpi-conf/agile-idm-core-conf.js
+++ rpi-conf/agile-idm-core-conf.js
@@ -3,199 +3,88 @@ var conf = {
   "storage": {
     "dbName": "/root/idm.db/database_"
   },
-  "upfront": {
-    pdp: {
-      ulocks: {
-        entityTypes: {
-          "/any": 0,
-          "/group": 1,
-          "/user": 2,
-          "/device": 3,
-          "/client": 4,
-          "/api": 5,
-          "/const": 6,
-          "/attr": 6,
-          "/prop": 6,
-          "/var": 6,
-        },
-        //fix this two eventually...
-        locks: "./node_modules/UPFROnt/example/online/Locks/",
-        actions: "./node_modules/UPFROnt/example/online/Actions"
-      }
-    },
-    pap: {
-      // this specifies host, port and path where
-      // this module should wait for requests
-      // if specified, the module runs as a PAP server
-      // if undefined, the module runs as a PAP client
-      // accessing another PAP server
-      /*server: {
-          "host": "localhost",
-          port: 1234,
-          path: "/pap/",
-          tls: false,
-          cluster: 1
-      },*/
-      // storage specifies where the policies
-      // are stored persistently:
-      // 1. if policies are stored remotely
-      // in another PAP, specify as type "remote"
-      // and indicate host, port and path
-      // 2. if policies are stored locally
-      // in a database, specify the db module
-      // ("mongodb", tbd) and the hostname and
-      // port
-      // thus, specifying type "remote" and specifying
-      // api yields an invalid configuration
-      storage: {
+  upfront_storage: {
         module_name: "agile-upfront-leveldb",
         type: "external",
         dbName: "/root/idm.db/pap_database",
         collection: "policies",
-        // specifies whether the module should check
-        // the cache to fetch a policy, of course,
-        // this may induce additional lookups but on
-        // average using the cache is recommended
-        cache: {
-          enabled: false,
-          TTL: 600,
-          pubsub: {
-            type: "redis",
-            channel: "policyUpdates"
-          }
-        }
-      }
-    }
-  },
+  }
+  ,
   "policies": {
     "create_entity_policy": [
       // actions of an actor are not restricted a priori
       {
-        target: {
-          type: "/any"
-        }
-      }, {
-        source: {
-          type: "/any"
-        }
+        op: "write"
+      },
+      {
+        op: "read"
       }
     ],
-    "top_level_policy": {
-      flows: [
+    "top_level_policy": [
+
         // all properties can be read by everyone
         {
-          target: {
-            type: "/any"
-          }
+          op: "read"
         },
         // all properties can only be changed by the owner of the entity
         {
-          source: {
-            type: "/user"
-          },
+          op: "write",
           locks: [{
+            lock: "hasType",
+            args: ["/user"]
+          }, {
             lock: "isOwner"
           }]
         },
         {
-          source: {
-            type: "/user"
-          },
+          op: "write",
           locks: [{
+            lock: "hasType",
+            args: ["/user"]
+          }, {
             lock: "attrEq",
             args: ["role", "admin"]
           }]
         }
 
       ],
-      actions: [{
-        name: "delete"
-      }]
+      //specify what should happen if the policy does not comply
+      actions: {
+        "read": [{
+          action: "delete"
+        }]
+      }
     },
     "attribute_level_policies": {
       "user": {
         "password": [
           // the property can only be read by the user itself
           {
-            target: {
-              type: "/user"
-            },
+            op: "read",
             locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
               lock: "isOwner"
-            }]
-          },
-          // the property can be set by the user itself and
-          {
-            source: {
-              type: "/user"
-            },
-            locks: [{
-              lock: "isOwner"
-            }]
-          },
-          // by all users with role admin
-          {
-            source: {
-              type: "/user"
-            },
-            locks: [{
-              lock: "attrEq",
-              args: ["role", "admin"]
             }]
           }
-        ],
-        "credentials": [
-          // the property can only be read by the user itself
-          {
-            target: {
-              type: "/any"
-            }
-          },
           // the property can be set by the user itself and
-          {
-            source: {
-              type: "/user"
-            },
+          , {
+            op: "write",
             locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
               lock: "isOwner"
             }]
           },
           // by all users with role admin
           {
-            source: {
-              type: "/user"
-            },
+            op: "write",
             locks: [{
-              lock: "attrEq",
-              args: ["role", "admin"]
-            }]
-          }
-        ],
-        "credentials.dropbox": [
-          // the property can only be read by the user itself
-          {
-            target: {
-              type: "/user"
-            },
-            locks: [{
-              lock: "isOwner"
-            }]
-          },
-          // the property can be set by the user itself and
-          {
-            source: {
-              type: "/user"
-            },
-            locks: [{
-              lock: "isOwner"
-            }]
-          },
-          // by all users with role admin
-          {
-            source: {
-              type: "/user"
-            },
-            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
               lock: "attrEq",
               args: ["role", "admin"]
             }]
@@ -204,16 +93,41 @@ var conf = {
         "role": [
           // can be read by everyone
           {
-            target: {
-              type: "/any"
-            }
+            op: "read"
           },
           // can only be changed by users with role admin
           {
-            source: {
-              type: "/user"
-            },
+            op: "write",
             locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "attrEq",
+              args: ["role", "admin"]
+            }]
+          }
+        ],
+        "credentials": [
+          // the property can only be read by the user itself
+          {
+            op: "read"
+          },
+          // the property can be set by the user itself and
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
+              lock: "isOwner"
+            }]
+          },
+          {
+            op: "write",
+            locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
               lock: "attrEq",
               args: ["role", "admin"]
             }]
@@ -221,58 +135,23 @@ var conf = {
         ]
       },
       "device": {
-        "credentials": {
-          flows: [
-            // all properties can be read by everyone
-            {
-              target: {
-                type: "/any"
-              }
-            },
-            // all properties can only be changed by the owner of the entity
-            {
-              source: {
-                type: "/user"
-              },
-              locks: [{
-                lock: "isOwner"
-              }]
-            }
-          ]
-        },
-        "credentials.dropbox": [
+        "credentials": [
           // the property can only be read by the user itself
           {
-            target: {
-              type: "/user"
-            },
-            locks: [{
-              lock: "isOwner"
-            }]
+            op: "read"
           },
           // the property can be set by the user itself and
           {
-            source: {
-              type: "/user"
-            },
+            op: "write",
             locks: [{
+              lock: "hasType",
+              args: ["/user"]
+            }, {
               lock: "isOwner"
-            }]
-          },
-          // by all users with role admin
-          {
-            source: {
-              type: "/user"
-            },
-            locks: [{
-              lock: "attrEq",
-              args: ["role", "admin"]
             }]
           }
         ]
       }
-
-    }
   },
   "schema-validation": [{
     "id": "/device",
