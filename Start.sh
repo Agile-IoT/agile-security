73b29208da21b47257a0c32fdf9a9417b092b9a6
david <nopbyte@users.noreply.github.com>
2017-08-17 14:16:31 +0200

    remove use of the pdp config.

--- Start.sh
+++ Start.sh
@@ -8,9 +8,5 @@ if [ ! -f "$CONF/agile-idm-core-conf.js" ]; then
   cp -r rpi-conf/* $CONF
 fi
 
-if [ ! -f "$CONF/agile-pdp-conf.js" ]; then
-  echo "folder not there for conf"
-  cp -r rpi-conf/agile-pdp-conf.js $CONF/agile-pdp-conf.js
-fi
 
 node /opt/agile-idm-web-ui/app.js $CONF
